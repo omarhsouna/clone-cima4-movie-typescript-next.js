9d9b76a7793cd5b1bc6e696b942c626fc99ec09f
omarhsouna <omar.hsouna@sesame.com.tn>
2023-01-11 08:44:32 +0100

    complete pagination

--- components/common/pagination.tsx
+++ components/common/pagination.tsx
@@ -21,8 +21,8 @@ const CircleButton = ({ isButton = false, handleClick, text }: PropsButton) => (
 );
 
 const Pagination = () => {
-  const pages = 3000;
-  const [selectedPage, setSelectedPage] = useState(3000);
+  const pages: number = 9;
+  const [selectedPage, setSelectedPage] = useState(1);
   return (
     <section className="w-full flex flex-wrap justify-center items-center gap-[14px]">
       {selectedPage !== 1 && (
@@ -39,10 +39,59 @@ const Pagination = () => {
         text="1"
         handleClick={() => setSelectedPage(1)}
       />
-      <CircleButton isButton text="2" />
-      <CircleButton isButton text="3" />
-      <CircleButton isButton text="4" />
-      <CircleButton text="..." />
+      {pages > 2 && (
+        <>
+          {/* ... */}
+          {selectedPage > 4 && <CircleButton text="..." />}
+          {/* ... */}
+          {/* before */}
+          {selectedPage > 2 && (
+            <>
+              <CircleButton
+                isButton
+                text={(selectedPage - 1).toString()}
+                handleClick={() => setSelectedPage(selectedPage - 1)}
+              />
+              {selectedPage - 1 > 2 && (
+                <CircleButton
+                  isButton
+                  text={(selectedPage - 2).toString()}
+                  handleClick={() => setSelectedPage(selectedPage - 2)}
+                />
+              )}
+            </>
+          )}
+          {/* before */}
+          {/* selected */}
+          {selectedPage < pages && selectedPage > 1 && (
+            <CircleButton text={selectedPage.toString()} />
+          )}
+          {/* selected */}
+
+          {/* after */}
+          {selectedPage < pages - 1 && (
+            <>
+              <CircleButton
+                isButton
+                text={(selectedPage + 1).toString()}
+                handleClick={() => setSelectedPage(selectedPage + 1)}
+              />
+              {selectedPage < pages - 2 && (
+                <CircleButton
+                  isButton
+                  text={(selectedPage + 2).toString()}
+                  handleClick={() => setSelectedPage(selectedPage + 2)}
+                />
+              )}
+            </>
+          )}
+          {/* after */}
+          {/* ... */}
+          {selectedPage < pages - 3 && <CircleButton text="..." />}
+          {/* ... */}
+        </>
+      )}
+
       <CircleButton
         isButton={pages !== selectedPage}
         text={pages.toString()}
