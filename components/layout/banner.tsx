dc5d2d2311002f91b358b674b846f06ebe137bb1
omarhsouna <omar.hsouna@sesame.com.tn>
2023-01-13 10:54:13 +0100

    add animation for banner for desktop

--- components/layout/banner.tsx
+++ components/layout/banner.tsx
@@ -15,12 +15,27 @@ const Banner = () => {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="bg-gradient-to-b from-[#3f255f] to-[#170231] min-h-[250px] flex  items-center justify-center w-full">
-      <h1 className="text-3xl font-bold text-white inline-block animatedText h-auto overflow-hidden typewriter animatedText">
+    <div className="bg-gradient-to-b from-[#3f255f] to-[#170231] min-h-[250px] flex items-center justify-center relative overflow-hidden">
+      <div className="absolute overflow-hidden translate-y-[-220px] wrapperText">
+        <div className="text-3xl text-yellow-400 opacity-0 translate-y-[-38px] firstText">
+          السينما للجميع
+        </div>
+        <span className="text-white text-7xl flex">
+          <span className="text-[105px] font-bold u">U</span>
+          <span className="text-[105px] font-bold text-yellow-400 four">4</span>
+          <span className="text-[105px] font-bold a">A</span>
+          <span className="text-[105px] font-bold m">M</span>
+          <span className="text-[105px] font-bold i">I</span>
+        </span>
+        <div className="text-lg text-white lastText">
+          لمشاهدة احدث الافلام و المسلسلات
+        </div>
+      </div>
+      <h1 className="text-3xl font-bold text-white animatedText typewriter animatedText">
         مشاهدة احدث الافلام و المسلسلات على{" "}
         {animatedText.split("").slice(0, count).join("")}
       </h1>
-      <div className="pacman mr-8">
+      <div className="pacman">
         <div className="before"></div>
         <div className="after"></div>
       </div>
@@ -29,8 +44,3 @@ const Banner = () => {
 };
 
 export default Banner;
-// <div className="text-3xl text-yellow-400">السينما للجميع</div>
-// <h1 className="text-white text-7xl">ImA4U</h1>
-// <span className="text-lg text-white ">
-//   لمشاهدة احدث الافلام و المسلسلات
-// </span>
